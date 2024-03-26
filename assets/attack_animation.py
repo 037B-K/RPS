cab635b4830907ef2bb02b77e40a8d564ba5830c
bertrandkuniedal <bertrandkuniedal@educ.college-montreal.qc.ca>
2024-03-26 10:42:31 -0400

    Initial commit

---
+++ assets/attack_animation.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class AttackType(Enum):
+   """
+   Simple énumération pour représenter les différents types d'attaques.
+   """
+   ROCK = 0,
+   PAPER = 1,
+   SCISSORS = 2
